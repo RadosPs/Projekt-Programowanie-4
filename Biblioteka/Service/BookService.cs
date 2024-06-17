d7763488aad966d363435f90c08c245b74468353
RadosPs <162735790+RadosPs@users.noreply.github.com>
2024-06-17 18:43:26 +0200

    Update BookService.cs

--- Biblioteka/Service/BookService.cs
+++ Biblioteka/Service/BookService.cs
@@ -1,12 +1,52 @@
-﻿using System;
+using Biblioteka.Model;
+using Biblioteka.Persistence;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+
 
 namespace Biblioteka.Service
 {
-    internal class BookService
+    public class BookService
     {
+        private readonly LibraryContext _context;
+
+        public BookService(LibraryContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Book>> GetAllBooksAsync()
+        {
+            return await _context.Books.Include(b => b.Category).ToListAsync();
+        }
+
+        public async Task<Book> GetBookByIdAsync(int id)
+        {
+            return await _context.Books.Include(b => b.Category).FirstOrDefaultAsync(b => b.Id == id);
+        }
+
+        public async Task AddBookAsync(Book book)
+        {
+            _context.Books.Add(book);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateBookAsync(Book book)
+        {
+            _context.Entry(book).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteBookAsync(int id)
+        {
+            var book = await _context.Books.FindAsync(id);
+            if (book != null)
+            {
+                _context.Books.Remove(book);
+                await _context.SaveChangesAsync();
+            }
+        }
     }
 }
