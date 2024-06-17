7454fab06e725ddb2fb1ce2705c7a0adc4c9bedc
RadosPs <162735790+RadosPs@users.noreply.github.com>
2024-06-17 18:51:25 +0200

    Add files via upload

---
+++ Biblioteka/ViewModels/BookCategoryViewModel.cs
@@ -0,0 +1,91 @@
+﻿using Biblioteka.Model;
+using Biblioteka.Service;
+using Biblioteka.ViewModels;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reactive;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Biblioteka.ViewModels
+{
+    public class BookCategoryViewModel : ViewModelBase
+    {
+        private readonly BookCategoryService _bookCategoryService;
+        private BookCategory _selectedCategory;
+        private string _categoryName;
+        private ObservableCollection<BookCategory> _categories;
+
+        public BookCategoryViewModel(BookCategoryService bookCategoryService)
+        {
+            _bookCategoryService = bookCategoryService;
+            LoadCategoriesCommand = ReactiveCommand.CreateFromTask(LoadCategoriesAsync);
+            AddCategoryCommand = ReactiveCommand.CreateFromTask(AddCategoryAsync);
+            UpdateCategoryCommand = ReactiveCommand.CreateFromTask(UpdateCategoryAsync);
+            DeleteCategoryCommand = ReactiveCommand.CreateFromTask(DeleteCategoryAsync);
+            LoadCategoriesCommand.Execute().Subscribe();
+        }
+
+        public ObservableCollection<BookCategory> Categories
+        {
+            get => _categories;
+            set => this.RaiseAndSetIfChanged(ref _categories, value);
+        }
+
+        public BookCategory SelectedCategory
+        {
+            get => _selectedCategory;
+            set => this.RaiseAndSetIfChanged(ref _selectedCategory, value);
+        }
+
+        public string CategoryName
+        {
+            get => _categoryName;
+            set => this.RaiseAndSetIfChanged(ref _categoryName, value);
+        }
+
+        public ReactiveCommand<Unit, Unit> LoadCategoriesCommand { get; }
+        public ReactiveCommand<Unit, Unit> AddCategoryCommand { get; }
+        public ReactiveCommand<Unit, Unit> UpdateCategoryCommand { get; }
+        public ReactiveCommand<Unit, Unit> DeleteCategoryCommand { get; }
+
+        private async Task LoadCategoriesAsync()
+        {
+            Categories = new ObservableCollection<BookCategory>(await _bookCategoryService.GetAllCategoriesAsync());
+        }
+
+        private async Task AddCategoryAsync()
+        {
+            var newCategory = new BookCategory
+            {
+                Name = CategoryName
+            };
+            await _bookCategoryService.AddCategoryAsync(newCategory);
+            await LoadCategoriesAsync();
+        }
+
+        private async Task UpdateCategoryAsync()
+        {
+            if (SelectedCategory != null)
+            {
+                SelectedCategory.Name = CategoryName;
+                await _bookCategoryService.UpdateCategoryAsync(SelectedCategory);
+                await LoadCategoriesAsync();
+            }
+        }
+
+        private async Task DeleteCategoryAsync()
+        {
+            if (SelectedCategory != null)
+            {
+                await _bookCategoryService.DeleteCategoryAsync(SelectedCategory.Id);
+                await LoadCategoriesAsync();
+            }
+        }
+    }
+}
---
+++ Biblioteka/ViewModels/CheckOutViewModel.cs
@@ -0,0 +1,126 @@
+﻿using Biblioteka.Model;
+using Biblioteka.Service;
+using Biblioteka.ViewModels;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reactive;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteka.ViewModels
+
+{
+    public class CheckOutViewModel : ViewModelBase
+    {
+        private readonly CheckOutService _checkOutService;
+        private CheckOut _selectedCheckOut;
+        private int _bookId;
+        private string _borrowerName;
+        private string _borrowerPhone;
+        private DateTime _borrowDate;
+        private DateTime _returnDate;
+        private ObservableCollection<CheckOut> _checkOuts;
+
+        public CheckOutViewModel(CheckOutService checkOutService)
+        {
+            _checkOutService = checkOutService;
+            LoadCheckOutsCommand = ReactiveCommand.CreateFromTask(LoadCheckOutsAsync);
+            AddCheckOutCommand = ReactiveCommand.CreateFromTask(AddCheckOutAsync);
+            UpdateCheckOutCommand = ReactiveCommand.CreateFromTask(UpdateCheckOutAsync);
+            DeleteCheckOutCommand = ReactiveCommand.CreateFromTask(DeleteCheckOutAsync);
+            LoadCheckOutsCommand.Execute().Subscribe();
+        }
+
+        public ObservableCollection<CheckOut> CheckOuts
+        {
+            get => _checkOuts;
+            set => this.RaiseAndSetIfChanged(ref _checkOuts, value);
+        }
+
+        public CheckOut SelectedCheckOut
+        {
+            get => _selectedCheckOut;
+            set => this.RaiseAndSetIfChanged(ref _selectedCheckOut, value);
+        }
+
+        public int BookId
+        {
+            get => _bookId;
+            set => this.RaiseAndSetIfChanged(ref _bookId, value);
+        }
+
+        public string BorrowerName
+        {
+            get => _borrowerName;
+            set => this.RaiseAndSetIfChanged(ref _borrowerName, value);
+        }
+
+        public string BorrowerPhone
+        {
+            get => _borrowerPhone;
+            set => this.RaiseAndSetIfChanged(ref _borrowerPhone, value);
+        }
+
+        public DateTime BorrowDate
+        {
+            get => _borrowDate;
+            set => this.RaiseAndSetIfChanged(ref _borrowDate, value);
+        }
+
+        public DateTime ReturnDate
+        {
+            get => _returnDate;
+            set => this.RaiseAndSetIfChanged(ref _returnDate, value);
+        }
+
+        public ReactiveCommand<Unit, Unit> LoadCheckOutsCommand { get; }
+        public ReactiveCommand<Unit, Unit> AddCheckOutCommand { get; }
+        public ReactiveCommand<Unit, Unit> UpdateCheckOutCommand { get; }
+        public ReactiveCommand<Unit, Unit> DeleteCheckOutCommand { get; }
+
+        private async Task LoadCheckOutsAsync()
+        {
+            CheckOuts = new ObservableCollection<CheckOut>(await _checkOutService.GetAllCheckOutsAsync());
+        }
+
+        private async Task AddCheckOutAsync()
+        {
+            var newCheckOut = new CheckOut
+            {
+                BookId = BookId,
+                BorrowerName = BorrowerName,
+                BorrowerPhone = BorrowerPhone,
+                BorrowDate = BorrowDate,
+                ReturnDate = ReturnDate
+            };
+            await _checkOutService.AddCheckOutAsync(newCheckOut);
+            await LoadCheckOutsAsync();
+        }
+
+        private async Task UpdateCheckOutAsync()
+        {
+            if (SelectedCheckOut != null)
+            {
+                SelectedCheckOut.BookId = BookId;
+                SelectedCheckOut.BorrowerName = BorrowerName;
+                SelectedCheckOut.BorrowerPhone = BorrowerPhone;
+                SelectedCheckOut.BorrowDate = BorrowDate;
+                SelectedCheckOut.ReturnDate = ReturnDate;
+                await _checkOutService.UpdateCheckOutAsync(SelectedCheckOut);
+                await LoadCheckOutsAsync();
+            }
+        }
+
+        private async Task DeleteCheckOutAsync()
+        {
+            if (SelectedCheckOut != null)
+            {
+                await _checkOutService.DeleteCheckOutAsync(SelectedCheckOut.Id);
+                await LoadCheckOutsAsync();
+            }
+        }
+    }
+}
