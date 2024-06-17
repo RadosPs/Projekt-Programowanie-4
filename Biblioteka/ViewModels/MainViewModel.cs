6553a6bce64f09f03eaa28d4d76f0ee9fcef0b68
RadosPs <162735790+RadosPs@users.noreply.github.com>
2024-06-17 18:50:24 +0200

    Update MainViewModel.cs

--- Biblioteka/ViewModels/MainViewModel.cs
+++ Biblioteka/ViewModels/MainViewModel.cs
@@ -1,6 +1,24 @@
-﻿namespace Biblioteka.ViewModels;
 
-public class MainViewModel : ViewModelBase
+
+using ReactiveUI;
+
+namespace Biblioteka.ViewModels
 {
-    public string Greeting => "Welcome to Avalonia!";
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private readonly BookViewModel _bookViewModel;
+        private readonly BookCategoryViewModel _bookCategoryViewModel;
+        private readonly CheckOutViewModel _checkOutViewModel;
+
+        public MainWindowViewModel(BookViewModel bookViewModel, BookCategoryViewModel bookCategoryViewModel, CheckOutViewModel checkOutViewModel)
+        {
+            _bookViewModel = bookViewModel;
+            _bookCategoryViewModel = bookCategoryViewModel;
+            _checkOutViewModel = checkOutViewModel;
+        }
+
+        public BookViewModel BookViewModel => _bookViewModel;
+        public BookCategoryViewModel BookCategoryViewModel => _bookCategoryViewModel;
+        public CheckOutViewModel CheckOutViewModel => _checkOutViewModel;
+    }
 }
